601aeeb9212e533aef866c3d39337c925f6f4c44
kenzonnn <kenza.benslimane@esprit.tn>
2025-02-20 10:54:45 -0300

    sign up for driver and owner

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
-require("dotenv").config(); // Load environment variables
+require("dotenv").config(); 
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected: ${conn.connection.name}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  userId: { type: String, required: true, unique: true },
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   phone: { type: Number, required: true },
-  vehicleType: { type: String, required: true },
-  role: { type: String, enum: ["Admin", "User"], default: "User" },
+  role: { type: String, enum: ["Owner", "Driver", "Admin", "Employe"], default: "Driver" },
+  vehicleType: { type: String, enum: ["Big", "Medium", "Small"], required: false },
 }, { timestamps: true });
 
 module.exports = mongoose.model("User", userSchema);
