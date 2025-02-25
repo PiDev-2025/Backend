9ef3fea8e42033f02a7286aa97a784d528ec1c26
kenzonnn <kenza.benslimane@esprit.tn>
2025-02-25 14:48:22 -0300

    fix user model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,19 +1,22 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({ 
+const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String },
   phone: { type: Number },
-  role: { type: String, enum: ["Owner", "Driver", "Admin", "Employe"]},
+  status:{type: String,
+    enum: ['Active', 'Blocked'],
+    required: false,
+    default: undefined
+  },
+  role: { type: String, enum: ["Owner", "Driver", "Admin", "Employe"] },
   vehicleType: {
     type: String,
-    enum: ['Moto', 'Citadine', 'Berline / Petit SUV', 'Familiale / Grand SUV','Utilitaire'],
+    enum: ['Moto', 'Citadine', 'Berline / Petit SUV', 'Familiale / Grand SUV', 'Utilitaire'],
     required: false,
-    default: undefined 
-},
-  otp: { type: String, required: false },
-  otpExpires: { type: Date, required: false },
+    default: undefined
+  }
 }, { timestamps: true });
 
 module.exports = mongoose.model("User", userSchema);
