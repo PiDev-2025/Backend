f026dc59611d102506cf27bf2180008bf27d6e30
Manai Yassine <98694575+YassineManai@users.noreply.github.com>
2025-05-07 00:45:21 +0100

    Update token.js
    add role to token to fix sign up

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 exports.generateToken = (user) => {
-  return jwt.sign({  id: user._id, name: user.name, email: user.email  }, process.env.JWT_SECRET, {
+  return jwt.sign({  id: user._id, name: user.name, email: user.email, role: user.role  }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
 };
